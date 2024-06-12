b6334fedb6944c0c624dfec0cc0e5cd2ed397990
Natália Silva <61921032+Nataliakrip@users.noreply.github.com>
2024-06-12 18:36:24 -0300

    Add files via upload
    feat: adicionando documento sql table users

---
+++ doc.sql.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `Users` (
+  `user_id` int PRIMARY KEY,
+  `department_id` int,
+  `username` varchar(50),
+  `passwordd` varchar(50),
+  `email` varchar(50),
+  `job_title` varchar(50),
+  `status` varchar(50),
+  `phone_Number` varchar(50)
+);
